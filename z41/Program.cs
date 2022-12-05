66f9b2b8e8b2522c0341200b5ba5fdbd3642e324
Vladislav <vladislav_chernikov94@mail.ru>
2022-12-05 22:51:35 +0700

    z41

---
+++ z41/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Write("Введите колличество чисел, которые хотите задать: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+    for (int i = 0; i < size; i++) 
+    {
+        array[i] = Int32.Parse(Console.ReadLine());
+    }
+
+int numbersGreaterZero(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    } return count;
+}
+
+int NumbersGreaterZero = numbersGreaterZero(array);
+Console.WriteLine($"Количество чисел больше 0 = {NumbersGreaterZero}");
